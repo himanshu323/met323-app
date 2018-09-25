9e95a56b2b0cc58c289a5e1008c45f75b17b512f
Himanshu Arora <himanshuarora323@gmail.com>
2018-09-25 23:59:15 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule, MatInputModule,MatFormFieldModule, MatCardModule, MatSelectModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatRadioModule, MatTableModule, MatTableDataSource, MatSidenavModule, MatIconModule} from "@angular/material"
-import { HeaderComponent } from 'src/app/header/header.component';
+import {MatToolbarModule, MatInputModule,MatFormFieldModule, MatCardModule, MatSelectModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatRadioModule, MatTableModule, MatTableDataSource, MatSidenavModule, MatIconModule, MatListModule} from "@angular/material"
+
 import { TradeCreateComponent } from 'src/app/trades/trade-create/trade-create.component';
 import { FormsModule } from '@angular/forms';
 import { TradeListComponent } from './trades/trade-list/trade-list.component';
@@ -15,11 +15,13 @@ import {FlexLayoutModule} from "@angular/flex-layout"
 import { TradeSearchComponent } from 'src/app/trades/trade-search/trade-search.component';
 import { TradeComponent } from 'src/app/trades/trade.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,HeaderComponent,TradeCreateComponent, TradeListComponent, 
-    TradeSearchComponent,TradeComponent
+    TradeSearchComponent,TradeComponent, SidenavListComponent
   ],
   imports: [
     BrowserModule,
@@ -39,6 +41,8 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
     FlexLayoutModule,
     MatSidenavModule,
     MatIconModule,
+    MatListModule,
+    
 
     AppRoutingModule
     
